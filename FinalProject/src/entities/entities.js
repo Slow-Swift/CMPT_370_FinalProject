89fbfa6cebd9bc4d22ef05c4e6e8262ef047e783
KennethHoesien <158524295+KennethHoesien@users.noreply.github.com>
2025-03-25 03:56:10 -0700

    I've made models.js and changed some of the functions for entities.js. The things I changed in the shaders are just from entities.vao/entities.components to entities.model.vao/entities.model.components

--- FinalProject/src/entities/entities.js
+++ FinalProject/src/entities/entities.js
@@ -6,8 +6,26 @@
  *   Creates entities that may be rendered by a shader
  */
 
+import { createModels } from "./models.js";
+
 let currentID = 1;
 
+// /**
+//  * Create a new entity given the indices, vertices, and texture coordinates
+//  * @param indices The indices of the model
+//  * @param vertices The vertex positions
+//  * @param textureCoords The texture coordinates for each vertex
+//  * @param texture The texture for the model
+//  * @returns The created entity
+//  */
+// export function createEntity(models) {
+//     const model = createModels(models)
+//     return { 
+//         model: model,
+//         id: currentID++,
+//         transform: createTransform(),
+//     };
+// }
 /**
  * Create a new entity given the indices, vertices, and texture coordinates
  * @param indices The indices of the model
@@ -16,18 +34,10 @@ let currentID = 1;
  * @param texture The texture for the model
  * @returns The created entity
  */
-export function createEntity(indices, vertices, textureCoords, normals, components) {
-    const vao = gl.createVertexArray();
-    gl.bindVertexArray(vao);
-    storeIndices(indices);
-    storeDataInAttribute(vertices, 0, 3);
-    storeDataInAttribute(textureCoords, 1, 2);
-    storeDataInAttribute(normals, 2, 3);
-
+export function createEntity(indices, vertices, orderedTextureCoords, orderedNormals, components) {
+    const model = createModels(indices, vertices, orderedTextureCoords, orderedNormals, components)
     return { 
-        vao: vao,
-        vertexCount: indices.length,
-        components: components,
+        model: model,
         id: currentID++,
         transform: createTransform(),
     };
@@ -40,10 +50,8 @@ export function createEntity(indices, vertices, textureCoords, normals, componen
  */
 export function copyEntity(entity) {
     return { 
-        vao: entity.vao,
-        vertexCount: entity.vertexCount,
-        components: entity.components,
         id: currentID++,
+        model: entity.model,
         transform: entity.transform.copy()
     };
 }
@@ -76,29 +84,4 @@ export function createTransform() {
             return transform;
         },
     }
-}
-
-/**
- * Create a new buffer and populate it with the provided data
- * @param data An array of floats to populate the buffer 
- * @returns The id of the the created buffer
- */
-function storeDataInAttribute(data, attribute, dimension) {
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer );
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-    gl.vertexAttribPointer(attribute, dimension, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(attribute);
-    return buffer;
-}
-
-/**
- * Create a new buffer and populate it with the indices data
- * @param indices An array of the indices
- * @returns The id of the the created buffer
- */
-function storeIndices(indices) {
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer );
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int32Array(indices), gl.STATIC_DRAW);
 }
